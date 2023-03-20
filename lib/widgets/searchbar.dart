af69628f28c8ca86fcc4686b84f8f524cc1e7ff2
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-20 21:23:32 +0200

    homepage search bar full

---
+++ lib/widgets/searchbar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+      child: TextField(
+        cursorColor: Colors.white,
+        decoration: InputDecoration(
+          suffixIcon: Container(
+            margin: const EdgeInsets.only(right: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: const Color.fromRGBO(243, 99, 31, 1),
+            ),
+            child: IconButton(
+              icon: const Icon(
+                Icons.search_rounded,
+                size: 30,
+              ),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+          filled: true,
+          fillColor: const Color.fromRGBO(113, 117, 128, 0.856),
+          hintText: "Podcast...",
+          hintStyle: const TextStyle(
+            color: Color.fromARGB(255, 221, 221, 221),
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
