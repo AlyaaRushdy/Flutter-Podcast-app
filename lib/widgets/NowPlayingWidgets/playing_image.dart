c966f71751b188a03d1ee83a7f97dd521749ebbc
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-22 01:52:26 +0200

    now playing image

---
+++ lib/widgets/NowPlayingWidgets/playing_image.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PlayingImage extends StatelessWidget {
+  const PlayingImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 45, bottom: 25),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            offset: const Offset(3, 10),
+            color: Colors.deepOrange.withOpacity(.5),
+          ),
+        ],
+      ),
+      child: Image.asset(
+        "images/bigimg.png",
+        width: 250,
+        height: 250,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}
