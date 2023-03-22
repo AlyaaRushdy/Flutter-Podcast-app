8178e59ba662bf66b33e53e200e8f371c7c84eac
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-22 03:34:19 +0200

    now playing full

--- lib/Screens/now_playing.dart
+++ lib/Screens/now_playing.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:podcast_app/widgets/NowPlayingWidgets/back_and_settings_row.dart';
+import 'package:podcast_app/widgets/NowPlayingWidgets/backward_pause_and_forward_row.dart';
 import 'package:podcast_app/widgets/NowPlayingWidgets/fav_download_and_share_row.dart';
+import 'package:podcast_app/widgets/NowPlayingWidgets/next_up.dart';
 import 'package:podcast_app/widgets/NowPlayingWidgets/playing_image.dart';
+import 'package:podcast_app/widgets/NowPlayingWidgets/time_slider.dart';
 import 'package:podcast_app/widgets/NowPlayingWidgets/title_and_channel_name.dart';
 
 class NowPlaying extends StatelessWidget {
@@ -10,18 +13,28 @@ class NowPlaying extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.only(top: 20),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 28),
-        child: Column(
-          children: [
-            const BackAndSettingsRow(),
-            const PlayingImage(),
-            const TitleAndChannelName(),
-            const SizedBox(height: 35),
-            const FavDownloadAndShareRow()
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 50),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: const [
+                BackAndSettingsRow(),
+                PlayingImage(),
+                TitleAndChannelName(),
+                SizedBox(height: 35),
+                FavDownloadAndShareRow(),
+                TimeSlider(),
+                BackwardsPauseAndForwardRow(),
+              ],
+            ),
+          ),
+          const NextUp(),
+        ],
       ),
     );
   }
