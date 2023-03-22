94a7bd3912a03965bf92e78c3d2693154ef8ce4a
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-22 03:03:19 +0200

    time slider

---
+++ lib/widgets/NowPlayingWidgets/time_slider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+
+class TimeSlider extends StatefulWidget {
+  const TimeSlider({
+    super.key,
+  });
+
+  @override
+  State<TimeSlider> createState() => _TimeSliderState();
+}
+
+class _TimeSliderState extends State<TimeSlider> {
+  double _value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 50, bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(_value.toStringAsFixed(2)),
+          Expanded(
+            child: SfSlider(
+              min: 0,
+              max: 21.43,
+              value: _value,
+              activeColor: Colors.deepOrange,
+              inactiveColor: Colors.grey,
+              thumbIcon: const Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 7.5,
+                ),
+              ),
+              onChanged: (dynamic value) {
+                setState(() {
+                  _value = value;
+                });
+              },
+            ),
+          ),
+          const Text("21:43"),
+        ],
+      ),
+    );
+  }
+}
